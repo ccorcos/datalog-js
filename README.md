d4658bf73c33ab7d382ad4cb005ee114612b4f90
Chet Corcos <ccorcos@gmail.com>
2018-12-20 23:00:25 -0800

    Tests and todos

--- README.md
+++ README.md
@@ -1,23 +1,39 @@
 # Datalog In JS
 
+This is an implementation of Datalogl, similar to DataScript and Datomic, but in TypeScript.
 
-database refactor
-- tests along the way. simpler types.
+## To Do
+
+- more tests
+- think about performance a little bit
+- explain option for query which says what happens
+
+- fix up the mess with keys - everything must be a string now, no numbers.
+- serialize and deserialize the separator character. maybe just use json stringify without the []
 
 - how can we verify the database?
-	- hash transactions?
-	- autoincrementing somehow?
+	- hash transactions ids?
 
-- build database basics, indexes, queries
-- test database funtionality
-- device and use identity model
-- database sync
+- how do we determine the "latest" transaction without an autoincrementing transaction id?
 
-- sync databases.
-- database as of transaction is an interesting pattern.
-- create a query language with a permissioning model.
+- query the historical database using transaction data.
+
+- download the music dataset and run through the datomic tutorials
+- short explainer about how this all works with resources on learning.
+
+- enforce uniqueness constraints
+- implement noHistory
+- redacting singular facts
+
+- abstractions for composing query functions
+
+- generalizable indexes
+	- custom indexes for n^2 queries
+
 - live query listeners.
-- multiple device sync.
+
+- database syncing
+	- authentication and permissioning
 
 ## Notes
 
--- test/demo.ts
+++ test/demo.ts
@@ -8,6 +8,7 @@ import * as level from "level"
 const db: LevelUp = level("test.db")
 
 test("set/get", async t => {
+	// Can we set and get?
 	await commit({
 		db: db,
 		schema: schema,
@@ -22,12 +23,86 @@ test("set/get", async t => {
 		where: [["who?", "person/name", "name?"]],
 	})
 
-	console.log("result", result)
+	t.is(result.length, 1)
+	t.is(result[0].length, 1)
 	t.is(result[0][0], "me")
+
+	// Can we overwrite facts?
+	await commit({
+		db: db,
+		schema: schema,
+		facts: [d("me", "person/name", "Chester Corcos")],
+	})
+
+	const result2 = await query({
+		db: db,
+		schema: schema,
+		find: ["name?"],
+		given: { "who?": ["me"] },
+		where: [["who?", "person/name", "name?"]],
+	})
+
+	t.is(result2.length, 1)
+	t.is(result2[0].length, 1)
+	t.is(result2[0][0], "Chester Corcos")
+
+	// Can we get multiple results?
+	await commit({
+		db: db,
+		schema: schema,
+		facts: [d("joe", "person/name", "Joeseph")],
+	})
+
+	const result3 = await query({
+		db: db,
+		schema: schema,
+		find: ["name?"],
+		given: {},
+		where: [["who?", "person/name", "name?"]],
+	})
+
+	t.is(result3.length, 1)
+	t.is(result3[0].length, 2)
+	t.deepEqual(result3[0], ["Joeseph", "Chester Corcos"])
+
+	// Can we get multi-valued results?
+	await commit({
+		db: db,
+		schema: schema,
+		facts: [
+			d("joe", "person/group", "g1"),
+			d("joe", "person/group", "g2"),
+			d("me", "person/group", "g1"),
+		],
+	})
+
+	const result4 = await query({
+		db: db,
+		schema: schema,
+		find: ["groups?"],
+		given: {},
+		where: [["joe", "person/group", "groups?"]],
+	})
+
+	t.is(result4.length, 1)
+	t.is(result4[0].length, 2)
+	t.deepEqual(result4[0], ["g1", "g2"])
+
+	const result5 = await query({
+		db: db,
+		schema: schema,
+		find: ["peeps?"],
+		given: {},
+		where: [["peeps?", "person/group", "g1"]],
+	})
+
+	t.is(result5.length, 1)
+	t.is(result5[0].length, 2)
+	t.deepEqual(result5[0], ["joe", "me"])
 })
 
 /*
-// Get list of conversations
+// Get list of convaersations
 query([
 	[chet, "group/name", UnknownGroup]
 ])
